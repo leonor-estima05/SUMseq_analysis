288c64279b0957454af9e0da92aa3e05a2f70cd3
leonor-estima05 <leonor.estima3@gmail.com>
2026-09-02 19:33:56 +0900

    Uncomment installation and data loading code

--- scRNA Scanpy.py
+++ scRNA Scanpy.py
@@ -19,7 +19,7 @@ sc.set_figure_params(dpi=100, facecolor="white")
 #RNA: barcodes.tsv(cell IDs), features.tsv(gene data), matrix.mtx and a merged seurat object .qs
 #ATAC: fragments as .bed.gz, ArchR .arrow (not scanpy!)
 
-#adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or other _filetype
+adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or other _filetype
 
 #samples = {
     #"ipsc mono": "file path to folder with the 3 RNA files for iPSCs",
