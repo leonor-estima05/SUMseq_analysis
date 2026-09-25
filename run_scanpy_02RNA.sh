989321879ff19360b0d2e47a7581607c6f14a057
leonor-estima05 <leonor.estima3@gmail.com>
2026-09-25 13:28:22 +0900

    Change script to run clustering instead of QC

---
+++ run_scanpy_02RNA.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH --mem=50GB
+#SBATCH --cpus-per-task=10
+#SBATCH --output=/home/projects/icell/le_RNA_analysis/scanpy/logs/scanpy_%j.out
+#SBATCH --error=/home/projects/icell/le_RNA_analysis/scanpy/logs/scanpy_%j.err
+
+set -eo pipefail
+
+source ~/.bashrc
+source "/home/l.estima/miniforge3/etc/profile.d/conda.sh"
+
+cd "/home/projects/icell/le_RNA_analysis/scanpy"
+
+conda activate scanpy_env
+
+export MPLBACKEND=Agg
+
+python -u 02_clustering_RNA.py
+
+conda deactivate
+
+
+
