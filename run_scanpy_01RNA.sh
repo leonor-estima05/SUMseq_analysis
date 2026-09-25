cb4b3ee1edeff3db829470e961f3b2989bfd3f3c
leonor-estima05 <leonor.estima3@gmail.com>
2026-09-25 12:47:23 +0900

    Modify resource allocation and logging in script
    Updated CPU allocation and log file paths in the script.

---
+++ run_scanpy_01RNA.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH --mem=50GB
+#SBATCH --cpus-per-task=10
+#SBATCH --output=/home/projects/icell/le_RNA_analysis/scanpy/logs/scanpy_%j.out
+#SBATCH --error=/home/projects/icell/le_RNA_analysis/scanpy/logs/scanpy_%j.out
+
+source ~/.bashrc
+source "/home/l.estima/miniforge3/etc/profile.d/conda.sh"
+
+cd "/home/projects/icell/le_RNA_analysis/scanpy"
+
+conda activate scanpy_env
+
+export MPLBACKEND=Agg
+
+python -u 01_qc_RNA.py
+
+conda deactivate
+
+
+
+
--- run_scanpy_RNA.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH --mem=50GB
-#SBATCH --cpus-per-task=20
-
-cd "/home/projects/icell/le_RNA_analysis/scanpy"
-
-
