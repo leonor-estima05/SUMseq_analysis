2f22d35cada283a34dbd80ca7f515b23c914555a
leonor-estima05 <leonor.estima3@gmail.com>
2026-09-25 11:34:48 +0900

    Refactor scRNA data processing and QC steps
    Updated data loading process and added quality control steps. Improved doublet removal and normalization procedures.

--- scRNA Scanpy.py
+++ scRNA Scanpy.py
@@ -3,7 +3,7 @@
 #Installed pooch (background manager for scanpy to import datasets)-not needed in actual data
 #Installed scikit-image
 #Installed scanpy[scrublet]
-#Installed harmonypy 0.0.6
+#Installed harmonypy 0.0.6 #will not be used because it makes little biological sense
 
 
 from __future__ import annotations
@@ -19,48 +19,47 @@ sc.set_figure_params(dpi=100, facecolor="white")
 #RNA: barcodes.tsv(cell IDs), features.tsv(gene data), matrix.mtx and a merged seurat object .qs
 #ATAC: fragments as .bed.gz, ArchR .arrow (not scanpy!)
 
-adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or other filetype
+base = "/home/l.estima/scanpy_input" #dir with individual sample mtx files (wrapped into .gz so read_10x_mtx will work)
 
-#samples = {
-    #"iMono: "file path to folder with the 3 RNA files for iPSCs",
-    #"cMo": "file path to folder with the 3 RNA files for monocytes",
-    #"intMo": "",
-    #"ncMo": "",
-#}
+samples = {
+    "iMono": f"{base}/iMono",
+    "cMo": f"{base}/cMo",
+    #"intMo": f"{base}/intMo", #intMo will not be run, at 3 median genes per cell it would produce too much noise.
+    "ncMo": f"{base}/ncMo",
+}
 
-#RNA_datasets = {}
+RNA_datasets = {}
 
-#for sample_id, filename in samples.items():
-    #sample_adata = sc.read_10x_mtx(path) #or _h5 depending on file format
-    #sample_adata.var_names_make_unique()
-    #RNA_datasets[sample_id] = sample_adata
+for sample_id, path in samples.items():
+    sample_adata = sc.read_10x_mtx(path) #or _h5 depending on file format
+    sample_adata.var_names_make_unique()
+    RNA_datasets[sample_id] = sample_adata
 
-#adata_RNA = ad.concat(RNA_datasets, label = "sample")
-#adata_RNA.obs_names_make_unique()
-#print(adata_RNA.obs["sample"].value_counts()) #shows how many cells per cell type
-#print(adata_RNA) #will show number of cells x genes
+adata_RNA = ad.concat(RNA_datasets, label = "sample", index_unique = "_")
+print(adata_RNA.obs["sample"].value_counts())
+print(adata_RNA) #will show number of cells x genes
 
 
 #2 QUALITY CONTROL
 #mostly uses sc.pp.calculate_qc_metrics()
 #measures mitochondrial, ribosomal and hemoglobin (important bc it would indicate RBC contamination)
 
-# mitochondrial genes, "MT-" for human, "Mt-" for mouse
-#adata_RNA.var["mt"] = adata.var_names.str.startswith("MT-")
-# ribosomal genes
-#adata_RNA.var["ribo"] = adata.var_names.str.startswith(("RPS", "RPL"))
+#mitochondrial genes, "MT-" for human, "Mt-" for mouse
+adata_RNA.var["mt"] = adata_RNA.var_names.str.startswith("MT-")
+#ribosomal genes
+adata_RNA.var["ribo"] = adata_RNA.var_names.str.startswith(("RPS", "RPL"))
 # hemoglobin genes
-#adata_RNA.var["hb"] = adata.var_names.str.contains("^HB[^(P)]")
+adata_RNA.var["hb"] = adata_RNA.var_names.str.contains("^HB[^(P)]")
 
-#sc.pp.calculate_qc_metrics(adata_RNA, qc_vars=["mt", "ribo", "hb"], inplace=True, log1p=True)
+sc.pp.calculate_qc_metrics(adata_RNA, qc_vars=["mt", "ribo", "hb"], inplace=True, log1p=True)
 
-#sc.pl.violin(
-    #adata_RNA,
-    #["n_genes_by_counts", "total_counts", "pct_counts_mt", "pct_counts_ribo", "pct_counts_hb""],
-    #groupby="sample",
-    #jitter=0.4,
-    #multi_panel=True,
-#)
+sc.pl.violin(
+    adata_RNA,
+    ["n_genes_by_counts", "total_counts", "pct_counts_mt", "pct_counts_ribo", "pct_counts_hb"],
+    groupby="sample",
+    jitter=0.4,
+    multi_panel=True,
+)
 
 #For my violin and scatter plots im seperating by cell type
 #Violin plots tell you # of genes expressed in count matrix, total counts per cell, % of counts in mitochondrial genes
@@ -72,78 +71,85 @@ adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or othe
 #Total Count: Want normal distribution. Outliers remove on either side.
 
 # mt vs total counts
-#sc.pl.scatter(adata_RNA, x='total_counts', y='pct_counts_mt', color='sample')
+sc.pl.scatter(adata_RNA, x='total_counts', y='pct_counts_mt', color='sample')
 # hb vs total counts
-#sc.pl.scatter(adata_RNA, x='total_counts', y='pct_counts_hb', color='sample')
+sc.pl.scatter(adata_RNA, x='total_counts', y='pct_counts_hb', color='sample')
 # ribo vs total counts
-#sc.pl.scatter(adata_RNA, x='total_counts', y='pct_counts_ribo', color='sample')
+sc.pl.scatter(adata_RNA, x='total_counts', y='pct_counts_ribo', color='sample')
 # genes vs total counts (for doublet detection)
-#sc.pl.scatter(adata_RNA, x='total_counts', y='n_genes_by_counts', color='sample')
+sc.pl.scatter(adata_RNA, x='total_counts', y='n_genes_by_counts', color='sample')
 
 #Based on the QC Metric plots filter out low quality cells/genes-change based on findings
-#They filtered cells with less than 100 genes expressed and genes detected in less than 3 cells
+#They filtered cells with less than 20 genes expressed and genes detected in less than 3 cells, this is low due to low sequencing depth
 #Set cell # by 0.1% of total cells
 
-#print(f"Before filtering: {adata_RNA.shape[0]} cells, {adata_RNA.shape[1]} genes")
+print(f"Before filtering: {adata_RNA.shape[0]} cells, {adata_RNA.shape[1]} genes")
 
 
 # filter by mitochondrial %
-#adata_RNA = adata_RNA[adata_RNA.obs['pct_counts_mt'] < 20] #cells with more than 20% mt genes
+adata_RNA = adata_RNA[adata_RNA.obs['pct_counts_mt'] < 20].copy() #cells with more than 20% mt genes
 # filter by hemoglobin %
-#adata_RNA = adata_RNA[adata_RNA.obs['pct_counts_hb'] < 1] #cells with more than 1% hb genes
-#sc.pp.filter_cells(adata_RNA, min_genes=100) #cells with less than 100 genes expressed
-#sc.pp.filter_genes(adata_RNA, min_cells=3) #genes in less than 3 cells
+adata_RNA = adata_RNA[adata_RNA.obs['pct_counts_hb'] < 1].copy() #cells with more than 1% hb genes
+sc.pp.filter_cells(adata_RNA, min_genes=20) #cells with less than 20 genes expressed, quite low due to low depth reads in this data
+sc.pp.filter_genes(adata_RNA, min_cells=3) #genes in less than 3 cells
 
-#print(f"After filtering: {adata_RNA.shape[0]} cells, {adata_RNA.shape[1]} genes")
-#print("Filtering complete")
+print(f"After filtering: {adata_RNA.shape[0]} cells, {adata_RNA.shape[1]} genes")
+print("Filtering complete")
 
 
 #3 DOUBLET REMOVAL (Warning: Scrublet takes a while)
 #Run doublet detection algorithm called Scrublet (Wolock et. al., 2019)
+#This will not be run on ncMo and cMo due to low median/cell gene counts
 
-#sc.pp.scrublet(adata_RNA, batch_key="sample")
-# filter out predicted doublets
-#adata_RNA = adata_RNA[adata_RNA.obs['predicted_doublet'] == False]
-#print(f"After doublet removal: {adata_RNA.shape[0]} cells")
+imono = adata_RNA[adata_RNA.obs["sample"] == "iMono"].copy()
+sc.pp.scrublet(imono)
+#will output predicted doublet scores
+adata_RNA.obs["predicted_doublet"] = False
+adata_RNA.obs.loc[imono.obs_names, "predicted_doublet"] = imono.obs["predicted_doublet"]
+adata_RNA.obs["doublet_score"] = float("nan")
+adata_RNA.obs.loc[imono.obs_names, "doublet_score"] = imono.obs["doublet_score"]
+print(f"Predicted doublets in iMono: {imono.obs["predicted_doublet"].sum()} of {imono.n_obs} cells")
 
 
 #Saving count data before normalization for DESeq2
 
-#adata_forDES = adata.copy()
-#adata_forDES.write_h5ad("adata_forDES.h5ad")
+adata_RNA_forDES = adata.copy()
+adata_RNA_forDES.write_h5ad("adata_forDES.h5ad")
+
+adata_RNA.layers["counts"] = adata_RNA.X.copy()
 
 #4 NORMALIZATION
 #Using count depth scaling + log + 1 transformation
 #Via target_sum in pp.normalize_total
 
-# Normalizing to median total counts
-#sc.pp.normalize_total(adata_RNA)
+#Normalizing to median total counts
+sc.pp.normalize_total(adata_RNA, target_sum=1e4)
 # Logarithmize the data
-#sc.pp.log1p(adata_RNA)
+sc.pp.log1p(adata_RNA)
 
 
 #5 FEATURE SELECTION
 #Used to include only most informative genes for dimensionality reduction
 #Uses scanpy function pp.highly_variable_genes
 
-#sc.pp.highly_variable_genes(adata_RNA, n_top_genes=2000, batch_key="sample")
-#sc.pl.highly_variable_genes(adata_RNA)
-# then filter to keep only highly variable genes
-#adata_RNA = adata_RNA[:, adata_RNA.var['highly_variable']]
+sc.pp.highly_variable_genes(adata_RNA, n_top_genes=500) #low top gene number used due to low count data
+sc.pl.highly_variable_genes(adata_RNA)
+# then filter to keep only highly variable genes, not doing this due to later gene expression analysis combined with low count data
+#adata_RNA = adata_RNA[:, adata_RNA.var['highly_variable']].copy()
 
 
 #6 PCA → DIMENSIONALITY REDUCTION
 #PCA reveals main axes of variation + does denoising
 
-#sc.tl.pca(adata_RNA)
-#sc.pl.pca_variance_ratio(adata_RNA, n_pcs=50, log=True) #variance plot
-#sc.pl.pca( # principal components plot
-    #adata_RNA,
-    #color=["sample", "sample", "pct_counts_mt", "pct_counts_mt"],
-    #dimensions=[(0, 1), (2, 3), (0, 1), (2, 3)],
-    #ncols=2,
-    #size=2,
-#)
+sc.tl.pca(adata_RNA, n_comps=30, use_highly_variable=True)
+sc.pl.pca_variance_ratio(adata_RNA, n_pcs=50, log=True) #variance plot
+sc.pl.pca( # principal components plot
+    adata_RNA,
+    color=["sample", "sample", "pct_counts_mt", "pct_counts_mt"],
+    dimensions=[(0, 1), (2, 3), (0, 1), (2, 3)],
+    ncols=2,
+    size=2,
+)
 
 #Reading the PCA Plots
 #Top 2 (Cells by gene expression): Each color is a sample (iPSC, classical, etc.)
@@ -160,23 +166,23 @@ adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or othe
 #7 NEAREST NEIGHBOR GRAPHS (UMAP)
 #If you inspect batch effects in your UMAP it can be beneficial
     #to integrate across samples and perform batch correction/integration.
-    #We recommend checking out scanorama and scvi-tools for batch integration.
 
-#sc.pp.neighbors(adata_RNA)
-#sc.tl.umap(adata_RNA)
-#sc.pl.umap(
-    #adata_RNA,
-    #color="sample",
-    #size=2,
-#)
+sc.pp.neighbors(adata_RNA, n_pcs=15)
+sc.tl.umap(adata_RNA)
+sc.pl.umap(
+    adata_RNA,
+    color=["sample", "total_counts", "n_genes_by_count", "pct_counts_mt"],
+    size=2,
+    ncols=2,
+)
 
 
 #8 CLUSTERING (LEIDEN)
 
 # Using the igraph implementation and a fixed number of iterations can be significantly faster,
 # especially for larger datasets
-#sc.tl.leiden(adata_RNA, flavor="igraph", n_iterations=2)
-#sc.pl.umap(adata_RNA, color=["leiden"])
+sc.tl.leiden(adata_RNA, flavor="igraph", n_iterations=2, directed=False)
+sc.pl.umap(adata_RNA, color=["leiden"])
 
 #Reading Leiden UMAP
     #Each dot = 1 cell
@@ -188,20 +194,21 @@ adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or othe
 #9 RE-ACCESS QUALITY CONTROL/FILTERING
 #Visualize QC metrics in UMAP
 
-#sc.pl.umap(
-    #adata,
-    #color=["leiden", "predicted_doublet", "doublet_score"],
-    # increase horizontal space between panels
-    #wspace=0.5,
-    #size=3,
-#)
+#BELOW UMAP DOES NOT APPLY TO ncMo and cMo
+sc.pl.umap(
+    adata_RNA,
+    color=["leiden", "predicted_doublet", "doublet_score"],
+    #increase horizontal space between panels
+    wspace=0.5,
+    size=3,
+)
 
-#sc.pl.umap(
-    #adata,
-    #color=["leiden", "log1p_total_counts", "pct_counts_mt", "log1p_n_genes_by_counts"],
-    #wspace=0.5,
-    #ncols=2,
-#)
+sc.pl.umap(
+    adata_RNA,
+    color=["leiden", "log1p_total_counts", "pct_counts_mt", "log1p_n_genes_by_counts"],
+    wspace=0.5,
+    ncols=2,
+)
 
 #Reading QC UMAPS
     #Doublet UMAP
@@ -213,66 +220,63 @@ adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or othe
         #Genes by count: Purple = few genes detected, yellow = many genes detected (very high count could indicate doublet)
 
 
-#Batch Correction (If needed)
-
-#sc.external.pp.harmony_integrate(adata_RNA, key='sample')
-#sc.pp.neighbors(adata_RNA, use_rep='X_pca_harmony')
-#sc.tl.umap(adata_RNA)
-#sc.tl.leiden(adata_RNA, resolution=0.5)
-#sc.pl.umap(adata_RNA, color=['leiden', 'sample'])
-
+#Batch Correction (If needed) NOT APPLICABLE HERE
 
 #Picking Ideal Resolution
 
-#sc.tl.leiden(adata_RNA, resolution=0.02, key_added='leiden_res_0.02')
-#sc.tl.leiden(adata_RNA, resolution=0.50, key_added='leiden_res_0.50')
-#sc.tl.leiden(adata_RNA, resolution=2.00, key_added='leiden_res_2.00')
+sc.tl.leiden(adata_RNA, resolution=0.02, key_added='leiden_res_0.02')
+sc.tl.leiden(adata_RNA, resolution=0.50, key_added='leiden_res_0.50')
+sc.tl.leiden(adata_RNA, resolution=2.00, key_added='leiden_res_2.00')
 
-#sc.pl.umap(
-    #adata_RNA,
-    #color=["leiden_res_0.02", "leiden_res_0.50", "leiden_res_2.00"],
-    #legend_loc="on data",
-#)
+sc.pl.umap(
+    adata_RNA,
+    color=["leiden_res_0.02", "leiden_res_0.50", "leiden_res_2.00"],
+    legend_loc="on data",
+)
 
 #10 MARKER GENE SET DEFINITION
 #use Buchrieser and calderon for iPSC mono and primary mono respectively
 #can also use CellMarker and PanglaoDB
 
-#marker_genes = {
-    #"iPSC mono": ["CD14", "FCGR3A", "CD163", "POU5F1", "SOX2", "NANOG", "KLF4", "DNMT38", "EPCAM"], #FCGR3A is CD16, POU5F1 is OCT4
-    #"classical mono": ["CD14", "S100A8", "S100A9", "CCR2", "LYZ"], #CellMarker 2.0 and Ziegler (for all)
-    #"intermediate mono": ["FCGR3A", "CD14", "HLA-DR", "MARCO"], #CellMarker 2.0
-    #"nonclassical mono": ["FCGR3A", "CD14", "HLA-DR", "ITGAM", "CX3CR1"], #ITGAM is CD11B
+marker_genes = {
+    "iMono": ["CD14", "FCGR3A", "CD163", "POU5F1", "SOX2", "NANOG", "KLF4", "DNMT3B", "EPCAM"], #FCGR3A is CD16, POU5F1 is OCT4
+    "cMo": ["CD14", "S100A8", "S100A9", "CCR2", "LYZ"], #CellMarker 2.0 and Ziegler (for all)
+    #"intermediate mono": ["FCGR3A", "CD14", "HLA-DR", "MARCO"], #CellMarker 2.0 #not used
+    "ncMo": ["FCGR3A", "CD14", "HLA-DRA", "ITGAM", "CX3CR1"], #ITGAM is CD11B
 
-#}
+}
 
-#sc.pl.dotplot(adata_RNA, marker_genes, groupby="leiden_res_0.5", standard_scale="var") #can change res
+all_genes = [g for gs in marker_genes.values() for g in gs]
+missing = [g for g in all_genes if g not in adata_RNA.var_names]
+print("Missing:", missing)
 
-#Then label according to cell type
+sc.pl.dotplot(adata_RNA, marker_genes, groupby="leiden_res_0.50", standard_scale="var") #can change res
 
-#adata.obs["cell_type_lvl1"] = adata.obs["leiden_res_0.02"].map( #ONLY KNOWN AFTER DOTPLOTS
-    #{
-        #"0": "iPSC mono",
-        #"1": "classical mono",
-        #"2": "intermediate mono",
-        #"3": "nonclassical mono",
-    #}
-#)
+sc.pl.dotplot(adata_RNA, marker_genes, groupby="sample", standard_scale="var")
 
-#sc.pl.dotplot(adata_RNA, marker_genes, groupby="leiden_res_0.50", standard_scale="var")
+imono_sub = adata_RNA[adata_RNA.obs["sample"] == "iMono"].copy()
+
+imono_panel = {
+    "monocyte": ["CD14", "FCGR3A", "LYZ", "S100A8", "CD163", "ITGAM"],
+    "pluripotency": ["POU5F1", "NANOG", "SOX2", "LIN28A", "DNMT3B"],
+    "proliferation": ["MKI67", "TOP2A"],
+}
+sc.pl.dotplot(imono_sub, imono_panel, groupby="leiden_res_0.50", standard_scale="var")
 
 #11 DIFFERENTLY EXPRESSED GENES AS MARKERS
 
 # Obtain cluster-specific differentially expressed genes
-#sc.tl.rank_genes_groups(adata_RNA, groupby="leiden_res_0.50", method="wilcoxon")
+sc.tl.rank_genes_groups(imono_sub, groupby="leiden_res_0.50", method="wilcoxon")
 
 #Visualize top 5 DEGs on dot plot
-#sc.pl.rank_genes_groups_dotplot(adata_RNA, groupby="leiden_res_0.50", standard_scale="var", n_genes=5)
+sc.pl.rank_genes_groups_dotplot(imono_sub, groupby="leiden_res_0.50", standard_scale="var", n_genes=5)
 
 #Can also get top DEGs as a table
-#sc.get.rank_genes_groups_df(adata_RNA, group="7").head(5)
+for cl in imono_sub.obs["leiden_res_0.50"].cat.categories:
+    print(f"--- cluster {cl} ---")
+    print(sc.get.rank_genes_groups_df(imono_sub, group=cl).head(5))
 
-#And Cluster DEGs
+#And Cluster DEGs #May be used after initial run
 #dc_cluster_genes = sc.get.rank_genes_groups_df(adata_RNA, group="7").head(5)["names"]
 #sc.pl.umap(
     #adata_RNA,
@@ -281,6 +285,7 @@ adata_RNA = sc.read_10x_mtx("file path to folder with the 3 RNA files") #or othe
     #frameon=False,
     #ncols=3,
 #)
+
 
 #then move onto DESeq2
 
